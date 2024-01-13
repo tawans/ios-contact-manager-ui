088752bc45b10dbe201098ec261687ab413456c3
Matthew(매튜) <krsder@naver.com>
2024-01-13 23:18:08 +0900

    feat: NavigationController 추가 및 ContactListController 기능 구현

--- ios-contact-manager/Controllers/ContactListViewController.swift
+++ ios-contact-manager/Controllers/ContactListViewController.swift
@@ -2,8 +2,13 @@ import UIKit
 
 class ContactListViewController: UIViewController {
     
-    var contactManager = ContactManager.shared
-    var contactListView: ContactListView = ContactListView()
+    let contactManager = ContactManager.shared
+    let contactListView: ContactListView = ContactListView()
+    
+    lazy var plusButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(naviTapped))
+        return button
+    }()
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -12,6 +17,9 @@ class ContactListViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError(CustomString.fatalErrorText.description)
     }
+}
+
+extension ContactListViewController {
     
     override func loadView() {
         view = contactListView
@@ -20,8 +28,43 @@ class ContactListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         contactListView.tableView.dataSource = self
+        setupNavigator()
+    }
+}
+
+extension ContactListViewController {
+    
+    func setupNavigator() {
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationController?.navigationBar.standardAppearance = navigationBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+        navigationController?.navigationBar.tintColor = .blue
+
+        navigationItem.scrollEdgeAppearance = navigationBarAppearance
+        navigationItem.standardAppearance = navigationBarAppearance
+        navigationItem.compactAppearance = navigationBarAppearance
+
+        navigationController?.setNeedsStatusBarAppearanceUpdate()
+
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.backgroundColor = .white
+        title = CustomString.contactListTitleText.description
+        
+        navigationItem.rightBarButtonItem = self.plusButton
     }
     
+    func reloadTableView() {
+        contactListView.tableView.reloadData()
+    }
+    
+    @objc func naviTapped() {
+        let detailVC = DetailContactViewController { [weak self] in
+            self?.reloadTableView()
+        }
+        let navigationController = UINavigationController(rootViewController: detailVC)
+        self.present(navigationController, animated: true)
+    }
 }
 
 extension ContactListViewController: UITableViewDataSource {
@@ -34,12 +77,12 @@ extension ContactListViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: CustomString.cellNameText.description, for: indexPath)
         let contactData = contactManager.readContact()
         let contact = contactData[indexPath.row]
-        var content = cell.defaultContentConfiguration()
+        var customCell = cell.defaultContentConfiguration()
         
-        content.text = "\(contact.name) (\(contact.age))"
-        content.secondaryText = contact.phoneNumber
+        customCell.text = "\(contact.name) (\(contact.age))"
+        customCell.secondaryText = contact.phoneNumber
         
-        cell.contentConfiguration = content
+        cell.contentConfiguration = customCell
         cell.accessoryType = .disclosureIndicator
         cell.selectionStyle = .none
         
