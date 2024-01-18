a7beaab736fb7ef4a60db8bf01e44c7340483abd
Matthew(매튜) <krsder@naver.com>
2024-01-18 21:10:15 +0900

    refactor: data의 cid 값 추가

--- ios-contact-manager/Models/Contact.swift
+++ ios-contact-manager/Models/Contact.swift
@@ -2,11 +2,15 @@ import UIKit
 
 struct Contact {
 
-    var name: String
-    var age: Int
-    var phoneNumber: String
+    static var nextId: Int = 0
+    let cid: Int
+    let name: String
+    let age: Int
+    let phoneNumber: String
 
     init(name: String, age: Int, phoneNumber: String) {
+        self.cid = Contact.nextId
+        Contact.nextId += 1
         self.name = name
         self.age = age
         self.phoneNumber = phoneNumber
