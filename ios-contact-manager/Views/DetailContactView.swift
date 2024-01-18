f60ed5a85fd06604e61d5955642557054601275b
Matthew(매튜) <krsder@naver.com>
2024-01-18 21:14:13 +0900

    refactor: 코드 리뷰 반영 코드베이스 AutoLayout refactoring

--- ios-contact-manager/Views/DetailContactView.swift
+++ ios-contact-manager/Views/DetailContactView.swift
@@ -2,106 +2,42 @@ import UIKit
 
 final class DetailContactView: UIView {
     
-    private let nameLabel: UILabel = {
-        let label = UILabel()
-        label.text = CustomString.nameText.description
-        label.textAlignment = .center
-        return label
-    }()
+    private var nameLabel: UILabel
+    private var ageLabel: UILabel
+    private var phoneNumberLabel: UILabel
     
-    let nameTextField: UITextField = {
-        let textField = UITextField()
-        textField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 6.0, height: 0.0))
-        textField.leftViewMode = .always
-        textField.textAlignment = .left
-        textField.autocapitalizationType = .none
-        textField.spellCheckingType = .no
-        textField.clearsOnBeginEditing = false
-        textField.layer.borderWidth = 1.0
-        textField.layer.borderColor = UIColor.lightGray.cgColor
-        textField.layer.cornerRadius = 3
-        textField.clipsToBounds = true
-        return textField
-    }()
+    var nameTextField: UITextField
+    var ageTextField: UITextField
+    var phoneNumberTextField: UITextField
     
-    private let ageLabel: UILabel = {
-        let label = UILabel()
-        label.text = CustomString.ageText.description
-        label.textAlignment = .center
-        return label
-    }()
+    var nameTextFieldView: UIStackView
+    var ageTextFieldView: UIStackView
+    var phoneNumberTextFieldView: UIStackView
     
-    let ageTextField: UITextField = {
-        let textField = UITextField()
-        textField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 6.0, height: 0.0))
-        textField.leftViewMode = .always
-        textField.textAlignment = .left
-        textField.autocapitalizationType = .none
-        textField.spellCheckingType = .no
-        textField.clearsOnBeginEditing = false
-        textField.layer.borderWidth = 1.0
-        textField.layer.borderColor = UIColor.lightGray.cgColor
-        textField.layer.cornerRadius = 3
-        textField.clipsToBounds = true
-        return textField
-    }()
-    
-    private let phoneNumberLabel: UILabel = {
-        let label = UILabel()
-        label.text = CustomString.phoneNumberText.description
-        label.textAlignment = .center
-        return label
-    }()
-    
-    lazy var phoneNumberTextField: UITextField = {
-        let textField = UITextField()
-        textField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 6.0, height: 0.0))
-        textField.leftViewMode = .always
-        textField.textAlignment = .left
-        textField.autocapitalizationType = .none
-        textField.spellCheckingType = .no
-        textField.clearsOnBeginEditing = false
-        textField.layer.borderWidth = 1.0
-        textField.layer.borderColor = UIColor.lightGray.cgColor
-        textField.layer.cornerRadius = 3
-        textField.clipsToBounds = true
-        textField.addTarget(self, action: #selector(phoneNumberEditingChanged(_:)), for: .editingChanged)
-        return textField
-    }()
-    
-    private lazy var nameTextFieldView: UIView = {
-        let view = UIView()
-        view.addSubview(nameLabel)
-        view.addSubview(nameTextField)
-        return view
-    }()
-    
-    private lazy var ageTextFieldView: UIView = {
-        let view = UIView()
-        view.addSubview(ageLabel)
-        view.addSubview(ageTextField)
-        
-        return view
-    }()
-    
-    private lazy var phoneNumberTextFieldView: UIView = {
-        let view = UIView()
-        view.addSubview(phoneNumberLabel)
-        view.addSubview(phoneNumberTextField)
-        return view
-    }()
-    
-    private lazy var stackView: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [nameTextFieldView, ageTextFieldView, phoneNumberTextFieldView])
-        stack.axis = .vertical
-        stack.distribution = .fillEqually
-        stack.alignment = .fill
-        stack.spacing = 14
-        return stack
-    }()
+    private var stackView: UIStackView
     
     override init(frame: CGRect) {
+
+        nameLabel = UILabel()
+        ageLabel = UILabel()
+        phoneNumberLabel = UILabel()
+
+        nameTextField = UITextField()
+        ageTextField = UITextField()
+        phoneNumberTextField = UITextField()
+        
+        nameTextFieldView = UIStackView()
+        ageTextFieldView = UIStackView()
+        phoneNumberTextFieldView = UIStackView()
+        stackView = UIStackView()
+        
         super.init(frame: frame)
+        
+        setupLabel()
+        setupTextField()
+        setupTextFieldView()
+        setupStackView()
+        
         setupUI()
         setupSubView()
         setupKeyboard()
@@ -109,15 +45,81 @@ final class DetailContactView: UIView {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError(CustomString.fatalErrorText.description)
+        fatalError(CustomViewControllerString.fatalErrorText.description)
     }
 }
-
+// MARK: Methed
 extension DetailContactView {
     
-    @objc
-    private func phoneNumberEditingChanged(_ textField: UITextField) {
-        textField.text = textField.text?.formatPhoneNumber()
+    private func setupLabel() {
+        nameLabel = configureLabel(with: CustomValidString.nameText.description)
+        ageLabel = configureLabel(with: CustomValidString.ageText.description)
+        phoneNumberLabel = configureLabel(with: CustomValidString.phoneNumberText.description)
+    }
+    
+    private func configureLabel(with text: String) -> UILabel {
+        let label: UILabel = UILabel()
+        label.text = text
+        label.textAlignment = .center
+        return label
+    }
+    
+    private func setupTextField() {
+        nameTextField = configureTextField()
+        ageTextField = configureTextField()
+        phoneNumberTextField = configureTextField()
+    }
+    
+    private func configureTextField() -> UITextField {
+        let textField: UITextField = UITextField()
+        textField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 6.0, height: 0.0))
+        textField.leftViewMode = .always
+        textField.textAlignment = .left
+        textField.autocapitalizationType = .none
+        textField.spellCheckingType = .no
+        textField.clearsOnBeginEditing = false
+        textField.layer.borderWidth = 1.0
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.cornerRadius = 3
+        textField.clipsToBounds = true
+        return textField
+    }
+    
+    private func setupTextFieldView() {
+        nameTextFieldView = configureTextFieldView(with: nameLabel, textField: nameTextField)
+        ageTextFieldView = configureTextFieldView(with: ageLabel, textField: ageTextField)
+        phoneNumberTextFieldView = configureTextFieldView(with: phoneNumberLabel, textField: phoneNumberTextField)
+    }
+    
+    private func configureTextFieldView(with label: UILabel, textField: UITextField) -> UIStackView {
+        let stack: UIStackView = UIStackView(arrangedSubviews: [label, textField])
+        stack.axis = .horizontal
+        stack.distribution = .fillProportionally
+        stack.alignment = .fill
+        stack.spacing = 14
+        
+        if label == nameLabel {
+            nameTextFieldView = stack
+        } else if label == ageLabel {
+            ageTextFieldView = stack
+        } else if label == phoneNumberLabel {
+            phoneNumberTextFieldView = stack
+        }
+        
+        return stack
+    }
+    
+    private func setupStackView() {
+        stackView = configureStackView()
+    }
+    
+    private func configureStackView() -> UIStackView {
+        let stack: UIStackView = UIStackView(arrangedSubviews: [nameTextFieldView, ageTextFieldView, phoneNumberTextFieldView])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.alignment = .fill
+        stack.spacing = 14
+        return stack
     }
     
     private func setupUI() {
@@ -134,53 +136,39 @@ extension DetailContactView {
         addSubview(stackView)
     }
     
-    private func setupConstraints() {
+    private func setupCustomAutoresizing() {
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameTextField.translatesAutoresizingMaskIntoConstraints = false
-        nameTextFieldView.translatesAutoresizingMaskIntoConstraints = false
         ageLabel.translatesAutoresizingMaskIntoConstraints = false
         ageTextField.translatesAutoresizingMaskIntoConstraints = false
-        ageTextFieldView.translatesAutoresizingMaskIntoConstraints = false
         phoneNumberLabel.translatesAutoresizingMaskIntoConstraints = false
         phoneNumberTextField.translatesAutoresizingMaskIntoConstraints = false
-        phoneNumberTextFieldView.translatesAutoresizingMaskIntoConstraints = false
         stackView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setupConstraints() {
+        let widthSize: CGFloat = 60
+        let heightSize: CGFloat = 34
         
-        nameLabel.centerYAnchor.constraint(equalTo: nameTextFieldView.centerYAnchor).isActive = true
-        nameLabel.leadingAnchor.constraint(equalTo: nameTextFieldView.leadingAnchor, constant: 8).isActive = true
-        nameLabel.trailingAnchor.constraint(equalTo: nameTextField.leadingAnchor, constant: -8).isActive = true
+        let sidePaddingSize: CGFloat = 14
+        let topPaddingSize: CGFloat = 20
         
-        nameTextField.topAnchor.constraint(equalTo: nameTextFieldView.topAnchor).isActive = true
-        nameTextField.bottomAnchor.constraint(equalTo: nameTextFieldView.bottomAnchor).isActive = true
-        nameTextField.leadingAnchor.constraint(equalTo: nameTextFieldView.leadingAnchor, constant: 70).isActive = true
-        nameTextField.trailingAnchor.constraint(equalTo: nameTextFieldView.trailingAnchor, constant: -8).isActive = true
+        nameLabel.widthAnchor.constraint(equalToConstant: widthSize).isActive = true
+        nameTextFieldView.heightAnchor.constraint(equalToConstant: heightSize).isActive = true
         
-        nameTextFieldView.heightAnchor.constraint(equalToConstant: 34).isActive = true
+        ageLabel.widthAnchor.constraint(equalToConstant: widthSize).isActive = true
+        ageTextFieldView.heightAnchor.constraint(equalToConstant: heightSize).isActive = true
         
-        ageLabel.centerYAnchor.constraint(equalTo: ageTextFieldView.centerYAnchor).isActive = true
-        ageLabel.leadingAnchor.constraint(equalTo: ageTextFieldView.leadingAnchor, constant: 8).isActive = true
-        ageLabel.trailingAnchor.constraint(equalTo: ageTextField.leadingAnchor, constant: -8).isActive = true
+        phoneNumberLabel.widthAnchor.constraint(equalToConstant: widthSize).isActive = true
+        phoneNumberTextFieldView.heightAnchor.constraint(equalToConstant: heightSize).isActive = true
         
-        ageTextField.topAnchor.constraint(equalTo: ageTextFieldView.topAnchor).isActive = true
-        ageTextField.bottomAnchor.constraint(equalTo: ageTextFieldView.bottomAnchor).isActive = true
-        ageTextField.leadingAnchor.constraint(equalTo: ageTextFieldView.leadingAnchor, constant: 70).isActive = true
-        ageTextField.trailingAnchor.constraint(equalTo: ageTextFieldView.trailingAnchor, constant: -8).isActive = true
-        
-        ageTextFieldView.heightAnchor.constraint(equalToConstant: 34).isActive = true
-        
-        phoneNumberLabel.centerYAnchor.constraint(equalTo: phoneNumberTextFieldView.centerYAnchor).isActive = true
-        phoneNumberLabel.leadingAnchor.constraint(equalTo: phoneNumberTextFieldView.leadingAnchor, constant: 8).isActive = true
-        phoneNumberLabel.trailingAnchor.constraint(equalTo: phoneNumberTextField.leadingAnchor, constant: -8).isActive = true
-        
-        phoneNumberTextField.topAnchor.constraint(equalTo: phoneNumberTextFieldView.topAnchor).isActive = true
-        phoneNumberTextField.bottomAnchor.constraint(equalTo: phoneNumberTextFieldView.bottomAnchor).isActive = true
-        phoneNumberTextField.leadingAnchor.constraint(equalTo: phoneNumberTextFieldView.leadingAnchor, constant: 70).isActive = true
-        phoneNumberTextField.trailingAnchor.constraint(equalTo: phoneNumberTextFieldView.trailingAnchor, constant: -8).isActive = true
-        
-        phoneNumberTextFieldView.heightAnchor.constraint(equalToConstant: 34).isActive = true
-        
-        stackView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 14).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -14).isActive = true
-        stackView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: sidePaddingSize).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -sidePaddingSize).isActive = true
+        stackView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: topPaddingSize).isActive = true
+    }
+    
+    @objc
+    private func phoneNumberEditingChanged(_ textField: UITextField) {
+        textField.text = textField.text?.formatPhoneNumber()
     }
 }
